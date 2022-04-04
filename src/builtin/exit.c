9c9545d3e32be850d95b8d08b54281dc89f64dc9
srngch <srngch.dev@gmail.com>
2022-04-05 00:39:12 +0900

    Update builtin exit - args can be out of 0~255 range

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -6,17 +6,24 @@
 /*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 00:04:18 by sarchoi           #+#    #+#             */
-/*   Updated: 2022/04/05 00:04:22 by sarchoi          ###   ########seoul.kr  */
+/*   Updated: 2022/04/05 00:26:29 by sarchoi          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	check_range(int n)
+static int	str_isdigit(char *str)
 {
-	if (n < 0 || 255 < n)
-		return (FT_ERROR);
-	return (FT_SUCCESS);
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (FT_FALSE);
+		i++;
+	}
+	return (FT_TRUE);
 }
 
 int	ft_exit(char **args)
@@ -26,22 +33,19 @@ int	ft_exit(char **args)
 		printf("exit\n");
 		exit(EXIT_SUCCESS);
 	}
-	if (args[0] && check_range(ft_atoi(args[0])) == FT_ERROR)
+	if (args[0] && str_isdigit(args[0]))
 	{
-		ft_putstr_fd("exit\nminishell: exit: ", 2);
-		ft_putstr_fd(args[0], 2);
-		ft_putstr_fd(": numeric argument required\n", 2);
-		exit(255);
+		printf("exit with number\n");
+		exit((unsigned char)ft_atol(args[0]));
 	}
 	if (args[0] && args[1])
 	{
 		ft_putstr_fd("exit\nminishell: exit: too many arguments\n", 2);
 		return (FT_ERROR);
 	}
-	if (args[0] && check_range(ft_atoi(args[0])) == FT_SUCCESS)
-	{
-		printf("exit with number\n");
-		exit((unsigned char)ft_atoi(args[0]));
-	}
-	return (FT_SUCCESS);
+	ft_putstr_fd("exit\nminishell: exit: ", 2);
+	ft_putstr_fd(args[0], 2);
+	ft_putstr_fd(": numeric argument required\n", 2);
+	exit(255);
+	return (FT_ERROR);
 }
