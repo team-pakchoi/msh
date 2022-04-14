30c9497dd1d586c2298f5a0f2d7d7c821db3efd2
srngch <srngch.dev@gmail.com>
2022-04-14 17:24:31 +0900

    Update builtin export for mutiple args

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,40 +6,49 @@
 /*   By: sarchoi <sarchoi@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/03 14:20:03 by sarchoi           #+#    #+#             */
-/*   Updated: 2022/04/05 17:19:55 by sarchoi          ###   ########seoul.kr  */
+/*   Updated: 2022/04/14 17:16:34 by sarchoi          ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_export(char **cmds)
+void	execute_export(char *name)
 {
 	t_var	*tmp;
 
+	if (!is_valid_var_name(name))
+	{
+		ft_putstr_fd("minishell: export: `", 2);
+		ft_putstr_fd(name, 2);
+		ft_putstr_fd("': not a valid identifier\n", 2);
+		g_mini.exit_status = 1;
+		return ;
+	}
+	tmp = find_var(name);
+	if (ft_strchr(name, '='))
+	{
+		if (tmp)
+			update_var(ft_strndup(name, ft_strchr(name, '=') - name), ft_strchr(name, '=') + 1);
+		else
+			add_var(name, ENV_VAR);
+		return ;
+	}
+	if (!tmp)
+		return ;
+	tmp->scope = ENV_VAR;
+}
+
+void	ft_export(char **cmds)
+{
 	g_mini.exit_status = 0;
 	if (!cmds[1])
 	{
 		ft_env();
 		return ;
 	}
-	if (!is_valid_var_name(cmds[1]))
+	while (*cmds)
 	{
-		ft_putstr_fd("minishell: export: `", 2);
-		ft_putstr_fd(cmds[1], 2);
-		ft_putstr_fd("': not a valid identifier\n", 2);
-		g_mini.exit_status = 1;
-		return ;
+		execute_export(*cmds);
+		cmds++;
 	}
-	tmp = find_var(cmds[1]);
-	if (ft_strchr(cmds[1], '='))
-	{
-		if (tmp)
-			update_var(ft_strndup(cmds[1], ft_strchr(cmds[1], '=') - cmds[1]), ft_strchr(cmds[1], '=') + 1);
-		else
-			add_var(cmds[1], ENV_VAR);
-		return ;
-	}
-	if (!tmp)
-		return ;
-	tmp->scope = ENV_VAR;
 }
