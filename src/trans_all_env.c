abff9ec1e07b6cfc986877d7090673560f33d6f2
PCHANUL <cpak@student.42seoul.kr>
2022-04-29 17:56:26 +0900

    Fix parse "echo $a$b"

--- src/trans_all_env.c
+++ src/trans_all_env.c
@@ -6,7 +6,7 @@
 /*   By: cpak <cpak@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/28 05:38:27 by cpak              #+#    #+#             */
-/*   Updated: 2022/04/28 06:59:33 by cpak             ###   ########seoul.kr  */
+/*   Updated: 2022/04/29 17:52:32 by cpak             ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,50 +38,41 @@ char	*change_str(char *str, char *str_tar, char *str_src)
 	return (new);
 }
 
-char	*trans_env_name_to_value(char *str, int start, int end, int *idx)
+char	*trans_env_name_to_value(char *str, int *start, int *end)
 {
 	char	*env_name;
 	char	*env_value;
 	char	*parsed_str;
 
-	env_name = ft_strndup(str + start, end - start);
+	env_name = ft_strndup(str + *start, *end - *start);
 	if (env_name[1] == '?')
 		env_value = ft_itoa((int)g_mini.exit_status);
 	else
 		env_value = find_var_value(env_name + 1);
 	parsed_str = change_str(str, env_name, env_value);
-	*idx = start + ft_strlen(env_value);
+	*start = *end + ft_strlen(env_value) - ft_strlen(env_name);
 	if (env_name[1] == '?')
 		free(env_value);
 	free(env_name);
 	return (parsed_str);
 }
 
+int	is_valid_env_name(char c)
+{
+	if (ft_isalpha(c) || ft_isdigit(c) || c == '_')
+		return (1);
+	return (0);
+}
+
 int	get_next_env_point(char *str, int *start, int *end)
 {
-	int	i;
-
-	i = 0;
-	*start = -1;
-	while (str[i])
-	{
-		if (str[i] == '$')
-			*start = i;
-		else if (*start != -1)
-		{
-			if (!ft_isalpha(str[i]) && !ft_isdigit(str[i]) && str[i] != '_')
-			{
-				if (i - *start == 1 && str[i] == '?')
-					i += 1;
-				*end = i;
-				return (1);
-			}
-		}
-		i += 1;
-	}
-	if (*start == -1)
+	while (str[*start] && str[*start] != '$')
+		*start += 1;
+	*end = *start + 1;
+	while (str[*end] && is_valid_env_name(str[*end]))
+		*end += 1;
+	if (!str[*start])
 		return (0);
-	*end = i;
 	return (1);
 }
 
@@ -89,12 +80,12 @@ int	trans_all_env(char **str)
 {
 	int	start;
 	int	end;
-	int	idx;
 
-	idx = 0;
-	while (get_next_env_point(*str + idx, &start, &end))
+	start = 0;
+	end = 0;
+	while (get_next_env_point(*str, &start, &end))
 	{
-		*str = trans_env_name_to_value(*str + idx, start, end, &idx);
+		*str = trans_env_name_to_value(*str, &start, &end);
 		if (*str == 0)
 			return (0);
 	}
