4699067fe3c841e6c7db89ccde14f3feb0f92e4b
PCHANUL <cpak@student.42seoul.kr>
2022-05-03 15:19:44 +0900

    Fix test func

--- test/test.sh
+++ test/test.sh
@@ -2,17 +2,28 @@ function test {
 	input="./test/$1.txt"
 	while IFS= read -r line
 	do
-		echo "$line >> result_bash" > tmp
-		bash -i < tmp
-		echo "$line >> result_mini" > tmp
-		./minishell < tmp
+		if [ $1 == "env" ]
+		then
+			echo "export TEST=BLA" > tmp
+			echo "$line >> result_bash" >> tmp
+			bash -i < tmp
+			echo "export TEST=BLA" > tmp
+			echo "$line >> result_mini" >> tmp
+			./minishell < tmp
+		else
+			echo "$line >> result_bash" > tmp
+			bash -i < tmp
+			echo "$line >> result_mini" > tmp
+			./minishell < tmp
+		fi
 	done < "$input"
 
 	name="result_$1.diff"
-	diff -u result_bash result_mini > $name
+	diff -u -s result_bash result_mini > $name
 	rm tmp result_bash result_mini
 }
 
+make re
 ls ./test | grep txt | sed s/.txt//g > test_files
 files="test_files"
 while IFS= read -r file
@@ -22,3 +33,6 @@ do
 done < "$files"
 
 rm test_files
+make fclean
+
+
